9faa937a56d2fbea59f96fe5ab4845e4f2c90a25
Kirralee west <110008741+west0418@users.noreply.github.com>
2022-10-10 12:53:01 +1030

    Added in comment

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012022S2FVLS1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012022S2FVLS1\n\nA docassemble extension.\n\n## Author\n\nKirralee west, west0418@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012022S2FVLS1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nKirralee west, west0418@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Kirralee west',
       author_email='west0418@flinders.edu.au',
